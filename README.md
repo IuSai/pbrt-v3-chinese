a252114c9f711d96061c4b8eb2041a6a73b2986b
shinganEuler <shingan.euler@gmail.com>
2023-02-28 14:24:44 +0800

    update

---
+++ README.md
@@ -0,0 +1,1 @@
+[Physically Based Rendering: From Theory To Implementation 3rd / 基于物理的渲染：从理论到实现第三版](https://shinganeuler.github.io/pbrt-v3-chinese/)
---
+++ actions/build/index.js
@@ -0,0 +1,28 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const markdown_to_github_pages = require("@shinganeuler/markdown-to-github-pages");
+const fs = require('fs');
+
+try {
+    const source = core.getInput("source");
+    const destination = core.getInput("destination");
+    console.log(`source ${source}!`);
+    console.log(`destination ${destination}!`);
+
+    if (fs.existsSync(source)) {
+        fs.readdir(source, (err, files) => {
+            if (err) throw err;
+
+            files.filter(file => path.extname(file) === '.md')
+                .forEach(file => {
+                    const filePath = path.join(source, file);
+                    markdown_to_github_pages.generateHtml(filePath, destination);
+                });
+        });
+
+        markdown_to_github_pages.generateIndex(destination);
+        markdown_to_github_pages.generateHtml(path.join(source, 'index.md'), destination);
+    }
+} catch (error) {
+    core.setFailed(error.message);
+}
